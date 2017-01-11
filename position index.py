1802c1a797654df54859f4441631182e89b81d79
johuotar <jmh@dnainternet.net>
2017-01-11 09:45:06 +0200

    progress

--- position index.py
+++ position index.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import codecs
 
-f=codecs.open('testfile.txt', 'r','UTF-8')
+f=codecs.open('testfile2.txt', 'r','UTF-8')
 words = f.read().splitlines()#remove end characters
 index = defaultdict(list)
 spacesindex = defaultdict(list)
@@ -16,27 +16,40 @@ def isBlank (myString):
 for pos, term in enumerate(words):
     spaces=len(term)-len(term.lstrip())
     if isBlank(term) ==False:
-        #remove indentation here, after counting them, before storing string
-        #remove 2 symbols * spaces and 2 from both ends?
-        
-        index[str(pos)].append(term[spaces:])#use spaces to slice beginning
-        
+        #use spaces to slice beginning characters
+        index[str(pos)].append(term[spaces:])
         spacesindex[pos].append(spaces)
         
     else:
         continue
 
+def search(values,searchFor):
+    for k in values:
+        for v in values[k]:
+            if searchFor in v:
+                print("found it at " + k)
+                return k
+            print (searchFor + " not found at " + k)
+            
+print (search(index,'polvi'))#returns relevant key
+#Choose word. TODO
+
+
+'''
+i=-1
+while i<=26:
+    i=i+1
+    i2=i+1#no support for indent amount variations atm
+    if spacesindex[i] < spacesindex[i2]:#Skip blanks here
+        print (str(i) + " upperclass of " + str(i2))#Test code pls ignore
+'''
 for key in index.keys():
     print ('key: ' + key + ' ' , end="")
     print('value: ', end="")
     print (index[key])
 
 for key in spacesindex.keys():
-    print('key ', end="")
-    print (key)
-    print('value ', end="")
+    print('key: ', end="")
+    print (key, end=" ")
+    print('value: ', end="")
     print (spacesindex[key])
-
-
-#for key in sorted(index):
-    #print (key, index[key])
